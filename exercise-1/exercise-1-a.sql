8173c24b5646ea4bdeaf1dc68c7a6db58c8ef9e4
NguyenNV <108649707+ngvnguyen22v6@users.noreply.github.com>
2025-03-24 08:22:09 +0700

    Delete exercise-1 directory

--- exercise-1/exercise-1-a.sql
+++
@@ -1,5 +0,0 @@
--- exercise-1-a.List UnitPrice & UnitsInStock from Products
--- EXPLAIN: a simple ProductName, UnitPrice, UnitsInStock from Products query
-
-SELECT ProductName, UnitPrice, UnitsInStock
-FROM Products
